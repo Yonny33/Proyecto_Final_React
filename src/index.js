05dc9816c7c972f5e5c198d151bf39438ff2baa7
Yonny33 <yonnisuarez33@gmail.com>
2021-06-27 18:06:03 -0500

    Proyecto_React.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Maps from './Maps';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Maps />, document.getElementById('Maps'));
+registerServiceWorker();
 
